ff26f973e3f7604a2af13c2f1bcae9abf07e78e4
Jobayer <jobayerrahman0220@gmail.com>
2023-05-18 15:34:54 +0600

    add header section

--- src/layout/Root.jsx
+++ src/layout/Root.jsx
@@ -1,5 +1,17 @@
+import { Outlet } from "react-router-dom";
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+
 const Root = () => {
-  return <div>Root</div>;
+  return (
+    <>
+      <Header />
+      <main className="flex-grow-1">
+        <Outlet />
+      </main>
+      <Footer />
+    </>
+  );
 };
 
 export default Root;
